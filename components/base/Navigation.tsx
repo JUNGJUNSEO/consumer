57071a29e845ba2a9218d10ba8ab6f6efb9895ad
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-17 22:52:43 +0900

    Create Navigation.tsx

---
+++ components/base/Navigation.tsx
@@ -0,0 +1,14 @@
+import classes from './Navigation.module.css'
+
+function Navigation() {
+
+
+    return (
+        <div className={classes.block}>
+
+        </div>
+    )
+}
+
+
+export default Navigation
