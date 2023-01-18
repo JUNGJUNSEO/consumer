e358922f1790a0b86ac31aed837ebd3a92af22ff
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-18 23:04:15 +0900

    Create Login.tsx

---
+++ components/login/Login.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from './Login.module.css'
+
+function Login(){
+
+    return (
+        <React.Fragment>
+            <div className={classes.title}>Login</div>
+            <form>
+                <div className={classes.control}>
+                    <input placeholder="Email" type="email" required />
+                </div>
+                <div className={classes.actions}>
+                    <input type="submit" value ="Login"/>
+                </div>
+                
+                    
+            </form>
+        </React.Fragment>
+            
+
+    )
+}
+
+
+export default Login
