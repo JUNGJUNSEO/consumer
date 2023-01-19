169725c36d6aa8836f069ff71f42717d51b3c9f2
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-19 22:57:42 +0900

    Create Modal.tsx

---
+++ components/ui/Modal.tsx
@@ -0,0 +1,44 @@
+import { Fragment, PropsWithChildren } from "react"
+import ReactDOM from "react-dom"
+import classes from './Modal.module.css'
+
+interface ModalProps {
+    onClose : () => void
+}
+
+const BackDrop = (props : ModalProps) => {
+    return (
+        <div className={classes.backdrop}  onClick={props.onClose}></div>
+    )
+}
+
+const ModalOverlay = () => {
+    return (
+        <div className={classes.modal}>
+            <div>
+                <form>
+                    <input type="text" placeholder="비교 상품 검색"></input>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+const Modal = (props : PropsWithChildren<ModalProps>) => {
+
+    const selectedElement = document.getElementById("modal")
+
+    if (selectedElement === null){
+        return <div></div>
+    }
+  
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(<BackDrop onClose = {props.onClose}/>, selectedElement)}
+            {ReactDOM.createPortal(<ModalOverlay/>, selectedElement)}
+        </Fragment>
+    )
+
+}
+
+export default Modal
