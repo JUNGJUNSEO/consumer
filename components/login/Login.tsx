e9a1170cea962855cabca9c672f51793c33e5fb5
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-21 23:36:17 +0900

    Update Login.tsx

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import classes from './Login.module.css'
+import LoginForm from "./LoginForm";
 
 function Login(){
 
     return (
-        <React.Fragment>
-            <div className={classes.title}>Login</div>
-            <form>
-                <div className={classes.control}>
-                    <input placeholder="Email" type="email" />
-                </div>
-                <div className={classes.actions}>
-                    <input type="submit" value ="Login"/>
-                </div>
+        // <React.Fragment>
+        //     <div className={classes.title}>Login</div>
+        //     <form>
+        //         <div className={classes.control}>
+        //             <input placeholder="Email" type="email" />
+        //         </div>
+        //         <div className={classes.actions}>
+        //             <input type="submit" value ="Login"/>
+        //         </div>
                 
                     
-            </form>
-        </React.Fragment>
+        //     </form>
+        // </React.Fragment>
+
+        <LoginForm/>
             
 
     )
