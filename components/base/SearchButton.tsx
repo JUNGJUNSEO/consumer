f11bedfc9c5678a12ef084213f0802889968895b
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-21 23:36:04 +0900

    Update SearchButton.tsx

--- components/base/SearchButton.tsx
+++ components/base/SearchButton.tsx
@@ -1,19 +1,23 @@
-import { useState } from "react"
+import { Fragment, useState } from "react"
 import Modal from "../ui/Modal"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faArrowLeft, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import classes from "./SearchButton.module.css"
+import Link from "next/link";
 
 function SearchButton(){
 
-    const [close, setClose] = useState(false)
+    const [close, setClose] = useState(true)
 
-    const closeHandler = () => {
+    const modalHandler = () => {
         setClose((pre) => !pre)
     }
 
     return (
-        <div>
-            {close && <Modal onClose = {closeHandler}/>}
-            <div onClick={closeHandler}>search</div>
-        </div>
+        <Fragment>
+            <FontAwesomeIcon icon={faMagnifyingGlass} onClick={modalHandler} className = {classes.button}/>
+            {!close && <Modal onClose = {modalHandler}/>}
+        </Fragment>
             
     )
 }
