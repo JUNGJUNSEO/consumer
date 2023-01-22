14ce9c19c31098dec334da09020b3841205ca4d8
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-22 18:05:25 +0900

    Update Header.tsx

--- components/base/Header.tsx
+++ components/base/Header.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router'
+import { Fragment } from 'react';
 import classes from './Header.module.css'
 import Logo from './Logo'
 import SearchButton from './SearchButton';
@@ -6,18 +8,24 @@ import SearchButton from './SearchButton';
 function Header() {
 
     const router = useRouter();
-
+    console.log(router.query)
     const loginHandler = () => {
         router.push('/login')
     }
 
     return (
+    
         <header className={classes.block}>
             <div className={classes.inner}>
                 <Logo/>
                 <div className={classes.items}>
                     <button>toggle button</button>
-                    <SearchButton/>
+                    <Link href="/?modal=search" as="/search">
+                        <SearchButton/>
+                    </Link>
+                    <Link href="/?modal=login" as="/login">
+                    
+                    </Link>
                     <button className={classes.button} onClick = {loginHandler}>
                         로그인
                     </button>
