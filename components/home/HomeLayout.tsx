58795967c3e2cae43700fd332a7f61ee8f3724fe
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-25 19:59:58 +0900

    Update HomeLayout.tsx

--- components/home/HomeLayout.tsx
+++ components/home/HomeLayout.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router"
 import { Fragment, PropsWithChildren } from "react"
+import LoginForm from "../login/LoginForm"
 import SearchInput from "../search/SearchInput"
 import Modal from "../ui/Modal"
 import classes from './HomeLayout.module.css'
@@ -7,13 +8,18 @@ import classes from './HomeLayout.module.css'
 function HomeLayout(props : PropsWithChildren) {
 
     let router = useRouter()
-
+    const closeHandler = () => {
+        router.push("/")
+    }
     return (
         <Fragment>
             {router.query.modal && (
-                <Modal onClose = {() => {router.push("/")}}/>
-                {router.query.modal === "search" && <SearchInput/>}
-                {router.query.modal === "login" && <SearchInput/>}
+                <>
+                    <Modal onClose={closeHandler} />
+                    {router.query.modal === "search" && <SearchInput/>}
+                    {router.query.modal === "login" && <LoginForm onClose = {closeHandler}/>}
+                </>
+                
              
             )}
             <main className={classes.main}>{props.children}</main>
