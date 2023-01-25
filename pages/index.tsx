e3229283f6915bd04b30b3cdadffe90cd196f5aa
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-25 20:00:54 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import MainTemplate from "@/components/main/MainTemplate"
 import Header from "@/components/base/Header"
 import HomeLayout from "@/components/home/HomeLayout"
+import TableItem from "@/components/table/TableItem"
 
 function Homepage(){
   return (
     <HomeLayout>
       <Header></Header>
+      <TableItem/>
     </HomeLayout>
       
    
