334f875b266c2b49522bd3af84bddb0be67b945b
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-28 12:47:49 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
+import Layout from '@/components/layout/Layout'
 import '@/styles/globals.css'
-// import '@fortawesome/fontawesome-svg-core/styles.css'
 import type { AppProps } from 'next/app'
-import { RecoilRoot } from 'recoil'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-   <RecoilRoot>
+   <Layout>
       <Component {...pageProps} />
-   </RecoilRoot> 
+   </Layout> 
   ) 
 }
