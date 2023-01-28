63e022acd4080265bed0684504fae5c763f9ac50
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-28 12:47:30 +0900

    Update SearchInput.tsx

--- components/search/SearchInput.tsx
+++ components/search/SearchInput.tsx
@@ -1,28 +1,16 @@
 import classes from "./SearchInput.module.css"
-import ReactDOM from "react-dom"
-import { Fragment } from "react"
-
-const SearchOverlay = () => {
-    return (
-        <div className={classes.block}>
-            <div>
-                <form>
-                    <input type="text" placeholder="비교 상품 검색"></input>
-                </form>
-            </div>
-        </div>
-    )
-}
+import {MdSearch} from "react-icons/md"
 
 
 function SearchInput() {
 
-    const selectedElement = document.getElementById("modal")
+    
 
     return (
-        <Fragment>
-            {ReactDOM.createPortal(<SearchOverlay/>, selectedElement)}
-        </Fragment>
+        <form className={classes.form}>
+            <MdSearch/>
+            <input type="text" placeholder="비교 상품 검색"></input>
+        </form>
         
 
     )
