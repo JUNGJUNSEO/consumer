16decc01f5e573822ffeb7ec1ed1a2caf4baa025
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-01-30 22:05:44 +0900

    Create Post.tsx

---
+++ components/post/Post.tsx
@@ -0,0 +1,13 @@
+import classes from "./Post.module.css"
+import Table from "./Table"
+
+function Post() {
+
+    return (
+        <div className={classes.block}>
+            <Table></Table>
+        </div>
+    )
+}
+
+export default Post
