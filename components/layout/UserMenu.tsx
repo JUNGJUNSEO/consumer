a904e19da9311ce64dc7feb9ec318ab5c3c31cd9
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-02 23:14:46 +0900

    Update UserMenu.tsx

--- components/layout/UserMenu.tsx
+++ components/layout/UserMenu.tsx
@@ -1,16 +1,40 @@
 import classes from "./Usermenu.module.css"
 import {SlLogout} from"react-icons/sl"
+import {CgProfile} from"react-icons/cg"
+import {BsCardHeading} from"react-icons/bs"
+import {AiFillHeart} from "react-icons/ai"
+import {GiBackwardTime} from "react-icons/gi"
+
 function UserMenu(){
 
     return (
         <div className={classes.block}>
-            <div className={classes.wrapper}>
-                <div>
+            <ul className={classes.wrapper}>
+                <li>
+                    <CgProfile/>
+                    내 프로필
+                </li>
+                
+                <hr></hr>
+                <li>
+                    <BsCardHeading/>
+                    내 포스트
+                </li>
+                <li>
+                    <AiFillHeart/>
+                    좋아요 표시한 포스트
+                </li>
+                <li>
+                    <GiBackwardTime/>
+                    최근에 읽은 포스트
+                </li>
+                <hr></hr>
+                <li>
                     <SlLogout/>
                     로그아웃
-                </div>
+                </li>
 
-            </div>
+            </ul>
         </div>
     )
 }
