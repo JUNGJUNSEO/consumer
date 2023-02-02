8350424537f4a6388165f6dd0375235329c881ed
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-02 23:15:08 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  experimental: {
+    scrollRestoration: true,
+  }
+
+}
