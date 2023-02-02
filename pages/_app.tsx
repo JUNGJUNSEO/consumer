30de40a8d447dc4d304d22519913e3cde17225d8
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-02 23:15:17 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,22 @@ import Layout from '@/components/layout/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+import {Noto_Sans_KR} from '@next/font/google'
 
+const notoSansKR = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout> 
+      <div className={notoSansKR.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout> 
+      </div>
+
     </RecoilRoot>
    
   ) 
