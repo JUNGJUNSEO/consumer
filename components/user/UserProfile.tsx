b5639b91e3246c2c57454b4c4a76fe186a5bd020
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-06 23:53:33 +0900

    Create UserProfile.tsx

---
+++ components/user/UserProfile.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link"
+import RoundButton from "../ui/Button/RoundButton"
+import styles from "./UserProfile.module.css"
+
+function UserProfile() {
+
+
+
+    return (
+        <div className={styles.block}>
+            <div className={styles.container}>
+                 <div className={styles.avatar}>
+                    <img src="https://codyhouse.co/app/assets/img/article-preview-img-author-1.svg"/>
+                 </div>
+                 <div className={styles.content}>
+                    <h1 className={styles.userName}>
+                        서정준
+                    </h1>
+                    <p className={styles.location}>
+                        경기
+                    </p>
+                    <RoundButton>
+                        <Link href={"/"}> 프로필 수정</Link>
+                    </RoundButton>
+
+                 </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+
+export default UserProfile
