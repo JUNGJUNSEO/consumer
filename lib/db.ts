ffcdf8a215681b57f4c902aee5b14f617aa8914b
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-12 22:30:21 +0900

    Create db.ts

---
+++ lib/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.DB_URL)
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+export default db;
