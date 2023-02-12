e0bf96a363e00ba461793fe0c78a3b1f833ec97e
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-12 22:30:18 +0900

    Create AuthFormContainer.tsx

---
+++ containers/AuthFormContainer.tsx
@@ -0,0 +1,42 @@
+import AuthForm from "@/components/auth/AuthForm"
+import { useRouter } from "next/router"
+import { PropsWithChildren, useState } from "react"
+
+interface AuthFormContainerProps {
+    onCloseAuthModal : () => void
+}
+
+function authFormContainer(props: PropsWithChildren<AuthFormContainerProps>){
+
+
+    const router = useRouter()
+
+    const [toggleState, setToggleState] = useState('LOGIN')
+
+    const ToggleClickHandler = () => {
+        setToggleState('JOIN')
+        router.push(`${router.pathname}?modal=join`)
+    }
+
+
+    return (
+        <AuthForm
+            onCloseAuthModal = {props.onCloseAuthModal}
+            onToggleClick = {ToggleClickHandler}
+            mode = {toggleState}
+        />
+
+    )
+
+
+}
+
+
+function validateEmail (email:string)  {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    return emailRegex.test(email)
+
+}
+
+
+export default authFormContainer
