d4811e4b7e9e1f268fb44d3d7aa53da7cd8681de
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-12 22:30:53 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,34 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 import {Noto_Sans_KR} from '@next/font/google'
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
+
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '900']
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <RecoilRoot>
       <div className={notoSansKR.className}>
         <Layout>
-          <Component {...pageProps} />
+          {Component.getLayout 
+            ? Component.getLayout(<Component {...pageProps}/>) 
+            : <Component {...pageProps}/>
+          }
+          
         </Layout> 
       </div>
 
