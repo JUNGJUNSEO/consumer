b701e559ab57c5d688676b79f3476f3ba25bb8f2
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-12 22:30:47 +0900

    Create like.tsx

---
+++ pages/[userId]/like.tsx
@@ -0,0 +1,16 @@
+import UserLayout from "@/components/user/UserLayout"
+
+const likePostPage = () => {
+
+    return (
+        <div>
+            내가 좋아하는 페이지
+        </div>
+    )
+
+}
+
+
+likePostPage.getLayout = UserLayout
+
+export default likePostPage
