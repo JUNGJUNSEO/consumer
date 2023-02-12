c47f013f6e1d95cf7ae81a6bd346168db9125c48
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-12 22:29:53 +0900

    Update NavProfile.tsx

--- components/user/NavProfile.tsx
+++ components/user/NavProfile.tsx
@@ -1,38 +1,59 @@
 import Link from "next/link"
+import Router, { useRouter } from "next/router"
+import { Fragment, useEffect, useState } from "react"
 import styles from "./NavProfile.module.css"
 
 function NavProfile() {
-
+    const [selectedOption, setSelectedOption] = useState('myposts');
+    const router = useRouter();
+ 
+    useEffect(() => {
+        const option = router.asPath.split('/').pop(); 
+        setSelectedOption(option)
+    }, [selectedOption])
 
     return (
-        <div className={styles.block}>
-            <div className={styles.container}>
-                <ul className={styles.navBar}>
-                    <li>
-                        <Link href = "/">
-                            <span className={styles.label}>내 포스트</span>
-                            <span className={styles.count}>0</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href = "/">
-                            <span className={styles.label}>좋아요 표시한 포스트</span>
-                            <span className={styles.count}>0</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href = "/">
-                            <span className={styles.label}>최근에 읽은 포스트</span>
-                            <span className={styles.count}>0</span>
-                        </Link>
-                    </li>
-            
-                </ul>
+        <Fragment>
+            <div className={styles.block}>
+                <div className={styles.container}>
+                    <ul className={styles.navBar}>
+                        <li>
+                            <Link href = {`${router.query.userId}`}>
+                                <span className={styles.label}>내 포스트</span>
+                                <span className={styles.count}>0</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href = {`${router.query.userId}/like`}>
+                                <span className={styles.label}>좋아요 표시한 포스트</span>
+                                <span className={styles.count}>0</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href = {`${router.query.userId}/read`}>
+                                <span className={styles.label}>최근에 읽은 포스트</span>
+                                <span className={styles.count}>0</span>
+                            </Link>
+                        </li>
+                
+                    </ul>
 
+
+                </div>
 
             </div>
 
-        </div>
+            {/* {selectedOption === 'myposts' && 
+                <div>seo</div>
+            }
+            {selectedOption === 'like' && 
+                <div>seo</div>
+            }
+            {selectedOption === 'read' && 
+                <div>seo</div>
+            } */}
+        </Fragment>
+
     )
 }
 
