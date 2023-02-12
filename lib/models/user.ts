8f924415e823959c45f36e61bed81cff258fe041
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-12 22:30:25 +0900

    Update user.ts

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true},
-    username: { type: String, required: true, unique: true},
-    password: { type: String, required: true, unique: true},
-    name: {type: String}
+    avatarUrl: String,
+    email: { type: String, required: true, unique: true },
+    socialOnly: { type: Boolean, default: false },
+    username: { type: String, required: true, unique: true },
+    password: { type: String },
+    name: { type: String },
+    location: String,
 })
 
 
