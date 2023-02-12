c4abe5e492a5a0bc26c912f254dc83871a55df60
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-12 22:31:02 +0900

    Update new-post.tsx

--- pages/new-post.tsx
+++ pages/new-post.tsx
@@ -1,24 +1,26 @@
-import TableForm from "@/components/post/TableForm";
+// import TableForm from "@/components/post/TableForm";
+
+import TableForm from "@/components/write/TableForm"
 
 function NewPostPage() {
 
-    const addPostHandler = async(data: any) => {
-        const response = await fetch('api/new-table', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+    // const addPostHandler = async(data: any) => {
+    //     const response = await fetch('api/new-table', {
+    //         method: 'POST',
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
 
-        console.log(await response.json())
-        console.log(data)
+    //     console.log(await response.json())
+    //     console.log(data)
 
-    } 
+    // } 
 
     return (    
-  
-        <TableForm onAddPost = {addPostHandler}/>
+        <TableForm/>
+        // <TableForm onAddPost = {addPostHandler}/>
     )
 }
 
