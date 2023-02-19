1a2b0f81a123de86f4a0908301b240899aa22fc0
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-19 22:44:49 +0900

    Update ProductsComparisonTable.tsx

--- components/post/ProductsComparisonTable.tsx
+++ components/post/ProductsComparisonTable.tsx
@@ -1,133 +1,89 @@
 import React, { useEffect } from "react";
 import styles from "./ProductsComparisonTable.module.css";
-  
-interface Product {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    image: string;
-}
-  
-const products: Product[] = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: 100,
-      description: "This is product 1",
-      image: "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg"
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: 200,
-      description: "This is product 2",
-      image: "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg"
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      price: 200,
-      description: "This is product 2",
-      image: "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg"
-    },
-    {
-      id: 4,
-      name: "Product 4",
-      price: 200,
-      description: "This is product 2",
-      image: "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg"
-    },
-    {
-      id: 5,
-      name: "Product 5",
-      price: 200,
-      description: "This is product 2",
-      image: "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg"
-    },
-    {
-      id: 6,
-      name: "Product 6",
-      price: 200,
-      description: "This is product 2",
-      image: "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg"
-    },
-    // Add more products as needed
-  ];
-  
-const ProductsComparisonTable: React.FC = () => {
+import {IoIosArrowBack, IoIosArrowForward} from "react-icons/io"
+import { AiFillCheckCircle } from "react-icons/ai";
 
-    const [currentProduct, setCurrentProduct] = React.useState(0);
+const products= [
+  ['이미지', "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg", "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg", "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg", "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg", "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg", "https://m.metashop.co.kr/web/product/big/202203/25e2ec04765cfe928145681aefa6b04f.jpg"],
+  ['가격', '100',  '200', '300', '400', '500', '600'],
+  ['제품명', "Product 1", "Product 1", "Product 1", "Product 1", "Product 1", "Product 1"],
+  
+  ['description', "This is product 1", "This is product 1", "This is product 1", "This is product 1", "This is product 1", "This is product 1"],
+];
+  
+const ProductsComparisonTable: React.FunctionComponent = () => {
 
-    const displayCount = 3
-    const showPrevBtn = currentProduct > 0
-    const showNextBtn = currentProduct < products.length - displayCount
-    
+    const [currentProduct, setCurrentProduct] = React.useState(1);
+    const displayCount = 4
+    const showPrevBtn = currentProduct > 1
+    const showNextBtn = currentProduct < products[0].length - displayCount
+
     const handlePrev = () => {
       setCurrentProduct(prevProduct => prevProduct - 1);  
     };
       
     const handleNext = () => {
       setCurrentProduct(prevProduct => prevProduct + 1);
-
-
     }
+
 
     return (
       <div className={styles.wrapper}>
         <table className={styles.table}>
           <thead>
             <tr>
-              <th className={styles.imageColumn} />
-              {products
-                .slice(currentProduct, currentProduct + displayCount)
-                .map((product) => {
-                  return(
-                  <th
-                    className="table-cell" 
-                    key={product.id}
-                  >
-                    {product.name}
-                  </th>
-                )})
-              }
+              <th>{products[0][0]}</th>
+              {products[0].slice(currentProduct, currentProduct + displayCount).map((cell, index) => (
+                <th key={index} >
+                  {index === 0 ? (
+                    <>
+                      <img src ={cell} alt = "this is the image" />
+                      <div className={styles.userPick}>
+                        <AiFillCheckCircle/>
+                        <span>사용자 pick!</span>
+                      </div>
+                    </>
+
+                  ):(
+                    <img src ={cell} alt = "this is the image"/>
+                  )}
+                  
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className={styles.dataColumn}>Image</td>
-              {products
-                .slice(currentProduct, currentProduct + displayCount)
-                .map((product) => (
-                  <th 
-                    className="table-cell" 
-                    key={product.id}
-                  >
-                    <img src = {product.image} alt = "this is the image"/>
-                  </th>
-                ))
-              }
-            </tr>
-           
-            
+            {products.slice(1).map((product, index) => (
+              <tr key={index}>
+                <td>{product[0]}</td>
+                {product.slice(currentProduct, currentProduct + displayCount).map((cell, index) => (
+                  <td key={index} >
+                    {cell}
+                  </td>
+                ))}
+
+              </tr>
+            ))}
+             
           </tbody>
         </table>
         <div className={styles.nav}>
-            <button 
-              className={styles.prevBtn}
-              style = {{visibility: showPrevBtn ? 'visible' : 'hidden'}} 
-              onClick={handlePrev}
-            >
-              Prev
-            </button>
-            <button 
-              className={styles.nextBtn}
-              style = {{visibility: showNextBtn ? 'visible' : 'hidden'}} 
-              onClick={handleNext}
-            >
-              Next
-            </button>
+          <button 
+            className={styles.prevBtn}
+            style = {{visibility: showPrevBtn ? 'visible' : 'hidden'}} 
+            onClick={handlePrev}
+          >
+            <IoIosArrowBack/>
+          </button>
+          <button 
+            className={styles.nextBtn}
+            style = {{visibility: showNextBtn ? 'visible' : 'hidden'}} 
+            onClick={handleNext}
+          >
+            <IoIosArrowForward/>
+          </button>
         </div>
+
       </div>
     );
 };
