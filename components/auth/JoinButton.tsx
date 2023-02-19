cd8eb3163bb7392917721bf08180af692831e0c6
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-19 22:44:07 +0900

    Create JoinButton.tsx

---
+++ components/auth/JoinButton.tsx
@@ -0,0 +1,13 @@
+import AuthButton from "../ui/Button/AuthButton"
+
+const JoinButton = () => {
+
+    return (
+        <AuthButton>
+            회원가입
+        </AuthButton>
+    )
+}
+
+
+export default JoinButton
