decde6d12059f35d302c4c5ad3db7fc40341f507
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-20 23:13:40 +0900

    Update user.ts

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -1,14 +1,35 @@
-import mongoose from 'mongoose'
+import mongoose, { Model, Schema, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema({
-    avatarUrl: String,
-    email: { type: String, required: true, unique: true },
-    socialOnly: { type: Boolean, default: false },
-    username: { type: String, required: true, unique: true },
-    password: { type: String },
-    name: { type: String },
-    location: String,
-})
+interface IUserDocument extends Document {
+  avatarUrl?: string;
+  email: string;
+  socialOnly?: boolean;
+  username: string;
+  password?: string;
+  name?: string;
+  location?: string;
+}
 
+interface IUserModel extends Model<IUserDocument> {}
 
-export default mongoose.model<typeof userSchema>('User', userSchema)
+const userSchema: Schema<IUserDocument, IUserModel> = new mongoose.Schema({
+  avatarUrl: String,
+  email: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, required: true, unique: true },
+  password: String,
+  name: String,
+  location: String,
+});
+
+userSchema.pre<IUserDocument>('save', function (next) {
+  if (this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, 5);
+  }
+  next();
+});
+
+const User: IUserModel = mongoose.models.User || mongoose.model<IUserDocument, IUserModel>('User', userSchema);
+
+export default User;
