fcb5183abdd333a5e0e64349a38e8a21a570dfec
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-20 23:13:13 +0900

    Update Post.tsx

--- components/post/Post.tsx
+++ components/post/Post.tsx
@@ -1,5 +1,6 @@
 import classes from "./Post.module.css"
 import PostBottom from "./PostBottom"
+import PostComment from "./PostComment"
 import PostHead from "./PostHead"
 import ProductsComparisonTable from "./ProductsComparisonTable"
 
@@ -10,6 +11,7 @@ function Post() {
             <PostHead/>
             <ProductsComparisonTable/>
             <PostBottom/>
+            <PostComment/>
         </div>
     )
 }
