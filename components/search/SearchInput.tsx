2d05f1878c3244250b676847c6a0726a046ce478
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-25 23:15:36 +0900

    Update SearchInput.tsx

--- components/search/SearchInput.tsx
+++ components/search/SearchInput.tsx
@@ -1,18 +1,19 @@
-import classes from "./SearchInput.module.css";
+import styles from "./SearchInput.module.css";
 import {MdSearch} from "react-icons/md";
-import {useRecoilValue} from "recoil";
-import { isPurpleAtom } from "@/lib/atom";
+
 
 
 function SearchInput() {
 
-    // const isPurple = useRecoilValue(isPurpleAtom)
-    // style={isPurple ? {border: "solid #734B87"}: {}}
     return (
-        <form className={classes.form}>
-            <MdSearch/>
-            <input type="text" placeholder="비교 상품 검색"></input>
-        </form>
+        <div className={styles.block}>
+            <form className={styles.form}>
+                <MdSearch/>
+                <input type="text" placeholder="비교 상품 검색"></input>
+            </form>
+
+        </div>
+
         
 
     )
