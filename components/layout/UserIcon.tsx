97bb7dd425ec62f668e49f9beb283f526792ec5c
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-25 23:15:09 +0900

    Update UserIcon.tsx

--- components/layout/UserIcon.tsx
+++ components/layout/UserIcon.tsx
@@ -1,10 +1,15 @@
 import classes from "./UserIcon.module.css"
 import { MdArrowDropDown } from "react-icons/md"
+import { PropsWithChildren } from "react"
 
-function UserIcon() {
+interface UserIconProp {
+    onClickMenu: () => void
+}
+
+function UserIcon(props:PropsWithChildren<UserIconProp>) {
 
     return (
-        <div className={classes.block}>
+        <div className={classes.block} onClick={props.onClickMenu}>
             <img 
                 src="https://codyhouse.co/app/assets/img/article-preview-img-author-1.svg"
                 alt="thumbnail"
