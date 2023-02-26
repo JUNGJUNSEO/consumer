46d6bbbf5c269ff1af5eee60336c8192712c261c
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-26 21:33:38 +0900

    Create join.tsx

---
+++ pages/join.tsx
@@ -0,0 +1,14 @@
+import JoinForm from "@/components/auth/JoinForm"
+import Post from "@/components/post/Post"
+
+function JoinPage() {
+
+
+    return (
+        <JoinForm/>
+        
+    )
+}
+
+
+export default JoinPage
