c6cc182ee56abe28ca367033bb643c0fbf7d9905
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-26 21:34:10 +0900

    Create WriteTitle.tsx

---
+++ components/write/WriteTitle.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react"
+import styles from "/WriteTitle.module.css"
+
+interface WriteTitleProps {
+    title: string
+    onChange: (event : React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const WriteTitle:FC<WriteTitleProps> = ({title, onChange}) => {
+
+    return (
+
+        <div>
+            <input 
+                type="text" 
+                value={title} 
+                onChange={onChange} 
+                placeholder = "제목을 입력하세요"
+            />
+        </div>
+    )
+
+}
+
+
+
+export default WriteTitle
