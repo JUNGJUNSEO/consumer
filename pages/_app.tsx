4be18213ab66bea2676277be3d315ae8f5563050
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-27 23:06:07 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,15 @@
-import Layout from '@/components/layout/Layout'
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { RecoilRoot } from 'recoil'
-import {Noto_Sans_KR} from '@next/font/google'
-import { ReactElement, ReactNode } from 'react'
-import { NextPage } from 'next'
-
+import { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import {Noto_Sans_KR} from '@next/font/google';
+import Layout from '@/components/layout/Layout';
+import { ReactElement, ReactNode, useEffect, useState } from 'react';
+import { NextPage } from 'next';
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '900']
-})
+});
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -21,20 +20,41 @@ type AppPropsWithLayout = AppProps & {
 }
 
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+
+  const [user, setUser] = useState({id:'', loggedIn: false})
+
+  useEffect(() => {
+
+    async function fetchUser() {
+      try {
+        const response = await fetch('/api/userinfo')
+        if (response.ok) {
+          const data = await response.json()
+          setUser(data)
+        }
+      } catch (error) {
+        setUser({id:'', loggedIn: false})
+      }
+    }
+    fetchUser()
+
+  }, [])
+
   return (
     <RecoilRoot>
       <div className={notoSansKR.className}>
-        <Layout>
+        <Layout user={user}>
           {Component.getLayout 
             ? Component.getLayout(<Component {...pageProps}/>) 
-            : <Component {...pageProps}/>
-          }
-          
+            : <Component {...pageProps}/>}
         </Layout> 
       </div>
-
     </RecoilRoot>
-   
   ) 
 }
+
+
+
+export default App;
