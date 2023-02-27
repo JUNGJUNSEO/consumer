a5fd7b65d7aab16c20d7a9ed7360a38fbd16b499
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-27 23:05:49 +0900

    Update post.ts

--- lib/models/post.ts
+++ lib/models/post.ts
@@ -1,22 +1,84 @@
-import mongoose, { Model, Schema, Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export interface IPost extends Document {
+  title: string;
+  owner_pick: number;
+  reason: string;
+  post_image: string;
+  files: string[];
+  text?: string;
+  texts?: string[][];
+  hashtags: string[];
+  createdAt: Date;
+  meta: {
+    likes: number;
+    comments_count: number;
+  };
+  comments: mongoose.Schema.Types.ObjectId[];
+  owner: mongoose.Schema.Types.ObjectId;
+}
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: true, trim: true, maxLength: 80 },
-    fileUrl: { type: String, required: true },
-    thumbUrl: { type: String, required: true },
-    description: { type: String, required: true, trim: true, minLength: 2},
-    createdAt: { type: Date, required: true, default: Date.now },
-    hashtags: [{ type: String, trim: true }],
-    meta: {
-      views: { type: Number, default: 0, required: true },
-      rating: { type: Number, default: 0, required: true },
+  title: {
+    type: String,
+    required: true,
+  },
+  owner_pick: {
+    type: Number,
+    required: true,
+  },
+  reason: {
+    type: String,
+    required: true,
+  },
+  post_image: {
+    type: String,
+    required: true,
+  },
+  files: {
+    type: [String],
+    required: true,
+  },
+  texts: {
+    type: [[String]],
+  },
+  hashtags: {
+    type: [String],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  meta: {
+    likes: {
+      type: Number,
+      default: 0,
+      required: true,
     },
-    comments: [
-      { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
-    ],
-    owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  });
+    comments_count: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'comment',
+    },
+  ],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+});
 
 
 
-export default User;
+const Post = mongoose.models.Post ||  mongoose.model<IPost>('Post', postSchema);
+
+export default Post;
