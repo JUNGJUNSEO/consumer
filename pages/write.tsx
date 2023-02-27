09666311e159dad33ab08d1ae867faaed585b178
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-27 23:06:25 +0900

    Update write.tsx

--- pages/write.tsx
+++ pages/write.tsx
@@ -1,10 +1,41 @@
-function WritePage() {
+import Post from "@/components/post/Post"
+import { withSessionSsr } from "@/lib/withSession"
+import { NextPage } from "next";
+
+interface WritePageProps {
+    user: {
+        loggedIn: boolean,
+        id: string
+    }
+}
+
+const WritePage: NextPage<WritePageProps> = ({ user }) => {
 
     return (
         <div>
-
+            <Post/>
         </div>
     )
 }
+export default WritePage;
 
-export default WritePage
+// export const getServerSideProps = withSessionSsr(
+//     async function getServerSideProps({ req }) {
+//       const user = req.session.user;
+    
+//       if (!user.loggedIn) {
+//         return {
+//             redirect: {
+//                 destination: "/",
+//                 permanent: false,
+//             },
+//         };
+//       }
+  
+//     return {
+//         props: {
+//             user: req.session.user,
+//         },
+//     };
+//     },
+// );
