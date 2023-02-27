afdbcc0187ba0cbb5df7fc2b5445fae52a28e9a1
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-02-27 23:05:16 +0900

    Update PostCard.tsx

--- components/common/PostCard.tsx
+++ components/common/PostCard.tsx
@@ -1,15 +1,15 @@
 import classes from "./PostCard.module.css"
 import { MdComment} from "react-icons/md"
 import {AiFillHeart} from "react-icons/ai"
-import { PartialPost } from "@/lib/graphql/post"
 import { useRouter } from "next/router"
 import TagList from "./TagList"
+import { IPost } from "@/lib/models/post"
 
 
 
 
 export type PostCardProps = {
-    post : PartialPost
+    post : IPost
 }
 
 function PostCard({post} : PostCardProps){
@@ -21,36 +21,34 @@ function PostCard({post} : PostCardProps){
     }
 
     return (
-        <div className={classes.post}>
+        <div className={classes.card}>
      
-            <div className={classes.image} onClick = {showDetailHandler}>
-                <img src={post.post_image} alt="product image"/>
-            </div>
+            <div className={classes.image} style={{backgroundImage: `url(${post.post_image})`}} onClick = {showDetailHandler}/>
        
             <div className={classes.content}>
-                <TagList tags = {post.tags}/>
+                <TagList tags = {post.hashtags}/>
                 <div className={classes.title}>
                     <h3><a href="#0">{post.title}</a></h3>
                 </div>
             
                 <div className={classes.author}>
                     <a href="#0">
-                        <img src={post.author_image} alt="product image"/>
+                        <img src="/images/deer.png" alt="product image"/>
                     </a>
 
                     <div className={classes.info}>
-                        <div className={classes.name}><a href="#0" rel="author">by {post.author}</a></div>
+                        <div className={classes.name}><a href="#0" rel="author">by {post.owner}</a></div>
                         <div className={classes.meta}>
-                            <span>{post.updated_at}</span>
+                            <span>{post.createdAt}</span>
                             <div>
                                 <div>
                                     <MdComment/>
-                                    <span>{post.comments_count}</span>
+                                    <span>{post.meta.comments_count}</span>
                                 </div>
 
                                 <div>
                                     <AiFillHeart/>
-                                    <span>{post.likes}</span>
+                                    <span>{post.meta.likes}</span>
                                 </div>
 
                             </div>
