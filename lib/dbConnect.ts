81b9fe1ae84e5675fb390054cfe3f09569431e05
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-02 22:42:25 +0900

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -6,13 +6,20 @@ async function dbConnect() {
     
     // MongoDB에 연결되어 있을 경우 또 다시 연결 되는 걸 막는다.
     if (connection) {
+        console.log('seo')
         return
     }
 
     // MongoDB와 Mongoose 연결
     try {
+        mongoose.set("strictQuery", false);
         connection = await mongoose.connect(process.env.DB_URL);
         const db = mongoose.connection
+
+        // User 모델과 Post 모델을 미리 로드합니다.
+        await import('./models/user');
+        await import('./models/post');
+
 
     } catch (error) {
         console.error('Error connecting to MongoDB:', error)
