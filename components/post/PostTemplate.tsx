18d34a9a3ed50e41881bd7dfe5b4a5c1f295a2cd
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-02 22:42:11 +0900

    Create PostTemplate.tsx

---
+++ components/post/PostTemplate.tsx
@@ -0,0 +1,33 @@
+import classes from "./PostTemplate.module.css"
+import PostAction from "./PostAction"
+import PostBottom from "./PostBottom"
+import PostComment from "./PostComment"
+import PostHead from "./PostHead"
+import ProductsComparisonTable from "./ProductsComparisonTable"
+import { ModelProps } from "@/types/model"
+
+interface PostTemplateProps extends ModelProps {
+}
+
+const PostTemplate:React.FC<PostTemplateProps> = ({user, post}) => {
+
+    return (
+        <div className={classes.block}>
+            <PostAction/>
+            <PostHead 
+                userId={user.username} 
+                title={post.title} 
+                createdAt={new Date(post.createdAt).toISOString()} 
+                tags={post.hashtags}
+            />
+            <ProductsComparisonTable
+                files={post.files}
+                texts={post.texts}
+            />
+            <PostBottom/>
+            <PostComment/>
+        </div>
+    )
+}
+
+export default PostTemplate
