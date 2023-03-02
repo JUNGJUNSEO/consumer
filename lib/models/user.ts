ae99ada9d02d955c4cdd80ca298d1c449d585777
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-02 22:42:31 +0900

    Update user.ts

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -1,41 +1,39 @@
-import mongoose, { Model, Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from 'mongoose';
 import bcrypt from 'bcrypt';
+import { IPost } from './post';
 
-interface IUserDocument extends Document {
+export interface IUser extends Document {
   avatarUrl?: string;
   email: string;
   socialOnly?: boolean;
   username: string;
-  password?: string;
+  password: string;
   name?: string;
   location?: string;
-  posts: mongoose.Schema.Types.ObjectId[];
+  posts: IPost['_id'][];
 }
 
-interface IUserModel extends Model<IUserDocument> {}
-
-const userSchema: Schema<IUserDocument, IUserModel> = new mongoose.Schema({
+const userSchema: Schema<IUser> = new mongoose.Schema({
   avatarUrl: String,
   email: { type: String, required: true, unique: true },
   socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: String,
-  name: String,
+  password: { type: String, required: true},
+  name: { type: String, required: true},
   location: String,
   posts: [{
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'post',
+    ref: 'Post',
   }]
 });
 
-userSchema.pre<IUserDocument>('save', function (next) {
+userSchema.pre<IUser>('save', function (next) {
   if (this.isModified('password')) {
     this.password = bcrypt.hashSync(this.password, 5);
   }
   next();
 });
 
-const User: IUserModel = mongoose.models.User || mongoose.model<IUserDocument, IUserModel>('User', userSchema);
+const User = mongoose.models.User as mongoose.Model<IUser> || mongoose.model<IUser>('User', userSchema);
 
 export default User;
