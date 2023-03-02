459a25aef44ab8b2f2b70cf161f7984d6a94c638
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-02 22:42:05 +0900

    Update PostHead.tsx

--- components/post/PostHead.tsx
+++ components/post/PostHead.tsx
@@ -1,18 +1,29 @@
 import TagList from "../common/TagList"
 import classes from "./PostHead.module.css"
+import { formatDate } from "@/lib/utils"
 
-function PostHead(){
+interface PostHeadProps{
+    userId: string
+    title: string
+    createdAt: string
+    tags: string[]
+}
+
+const PostHead:React.FC<PostHeadProps> = ({userId, title, createdAt, tags}) => {
+    console.log(userId)
 
     return (
         <div className={classes.block}>
             <div className={classes.wrapper}>
-                <h1 className={classes.title}>내가 사용한 세탁 세제 전격 비교</h1>
+                <h1 className={classes.title}>{title}</h1>
                 <div className={classes.info}>
-                    <div>By <a>JungJunSeo</a></div>
+                    <div>
+                        By <a>{userId}</a>
+                    </div>
                     <div style={{ borderLeft: "2px solid",  height: "16px", margin: "0 1rem"}} />
-                    <span>어제 작성</span>
+                    <span>{formatDate(createdAt)}</span>
                 </div>
-                <TagList tags = {['빛나라 세탁 세제', '솔티드 카라멜 세탁 세제', '찌든떄 팍팍 세제']}/>
+                <TagList tags = {tags}/>
 
             </div>
                 
