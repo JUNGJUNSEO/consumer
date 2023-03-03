3e2b10340ae18e4fd5b3382723b320333ce11b55
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-03 23:40:34 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,13 @@
 import PostCardGrid from "@/components/common/PostCardGrid";
-import ProductsComparisonTable from "@/components/post/ProductsComparisonTable";
 import dbConnect from "@/lib/dbConnect";
-import { PartialPost } from "@/lib/graphql/post";
-import { middleware } from "@/lib/middleware";
 import Post, { IPost } from "@/lib/models/post";
-import { GetServerSideProps } from "next";
-
-
-
-const posts = [
-  {
-    id: "m1",
-    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-    tags: ["삼성", "LG"],
-    post_image: "https://codyhouse.co/app/assets/img/article-preview-img-1.jpg",
-    author_image: "https://codyhouse.co/app/assets/img/article-preview-img-author-1.svg",
-    updated_at: "2023-01-26",
-    comments_count: 10,
-    likes: 10,
-    is_private: false,
-    author: "Olivia Gribben"
-  },
-
-  {
-    id: "m2",
-    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-    tags: ["삼성", "LG"],
-    post_image: "https://codyhouse.co/app/assets/img/article-preview-img-1.jpg",
-    author_image: "https://codyhouse.co/app/assets/img/article-preview-img-author-1.svg",
-    updated_at: "2023-01-26",
-    comments_count: 10,
-    likes: 10,
-    is_private: false,
-    author: "Olivia Gribben"
-  },
-
-  {
-    id: "m3",
-    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-    tags: ["삼성", "LG"],
-    post_image: "https://codyhouse.co/app/assets/img/article-preview-img-1.jpg",
-    author_image: "https://codyhouse.co/app/assets/img/article-preview-img-author-1.svg",
-    updated_at: "2023-01-26",
-    comments_count: 10,
-    likes: 10,
-    is_private: false,
-    author: "Olivia Gribben"
-  },
-
-  {
-    id: "m4",
-    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-    tags: ["samsung", "LG"],
-    post_image: "https://codyhouse.co/app/assets/img/article-preview-img-1.jpg",
-    author_image: "https://codyhouse.co/app/assets/img/article-preview-img-author-1.svg",
-    updated_at: "2023-01-26",
-    comments_count: 10,
-    likes: 10,
-    is_private: false,
-    author: "Olivia Gribben"
-  },
-
-];
-
+import { GetServerSideProps, NextPage} from "next";
 
 export type PostCardProps = {
   posts : IPost[]
 }
 
-
-function Homepage({posts} : PostCardProps){
+const Homepage: NextPage<PostCardProps> = ({ posts }) => {
   return (
     <>
       <PostCardGrid posts={posts}/>
@@ -78,12 +16,12 @@ function Homepage({posts} : PostCardProps){
   )
 }
 
-
 export const getServerSideProps: GetServerSideProps = async(context) => {
 
   try {
     await dbConnect()
-    const posts: IPost[] = await Post.find({})
+    const posts = await Post.find({}).populate('owner')
+
 
     return {
       props: {
@@ -92,10 +30,13 @@ export const getServerSideProps: GetServerSideProps = async(context) => {
     };
   } catch (error){
     console.log(error)
+    return {
+      props: {
+        posts: []
+      }
+    }
   }
 
-
 }
-
 
 export default Homepage
