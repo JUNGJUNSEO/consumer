7672566bffd065a38c4c17467194b7367c05543d
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-03 23:40:13 +0900

    Create index.tsx

---
+++ pages/[userId]/[postTitle]/index.tsx
@@ -0,0 +1,75 @@
+import PostTemplate from "@/components/post/PostTemplate"
+import dbConnect from "@/lib/dbConnect"
+import User, { IUser } from "@/lib/models/user"
+import { ModelProps } from "@/types/model";
+import { GetStaticPaths } from "next"
+
+interface PostPageProps extends ModelProps {
+}
+
+interface IPath {
+  params: {
+      userId: string,
+      postTitle: string
+  }
+}
+
+export default function PostPage({ user, post }: PostPageProps) {
+
+    
+  return (
+      <PostTemplate user={user} post={post} />
+  )
+}
+
+
+export const getStaticPaths: GetStaticPaths = async(res) => {
+    try {
+        await dbConnect()
+    
+        const users: IUser[] =  await User.find({}).populate('posts', 'title');
+        
+        const paths = users.flatMap((user) => {
+          if (user.posts) {
+            return user.posts.map((post) => ({
+              params: { userId: user.username, postTitle: post.title }
+            }))
+          } else {
+            return []
+          }
+        })
+   
+        return { paths, fallback: false };
+
+    } catch(error) {
+        console.log(error)
+        return { paths: [], fallback: false }
+    }
+
+}
+
+export const getStaticProps = async({ params }: IPath) => {
+
+    try {
+      await dbConnect()
+
+      const { userId, postTitle } = params
+
+      const user = await User.findOne({ username: userId }).populate('posts');
+      const post = user.posts.find((post) => post.title === postTitle);
+      const data = { user, post }
+      const newData = JSON.parse(JSON.stringify(data))
+
+      return {
+        props: {
+          user: newData.user,
+          post: newData.post,
+        },
+      };
+    } catch (error){
+      console.log(error)
+      return {
+        notFound: true
+      }
+    }
+}
