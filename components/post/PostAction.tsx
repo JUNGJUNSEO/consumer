32fb3a7b85287cbba4a68ee5c945e9468e0ee15e
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-03 23:39:51 +0900

    Update PostAction.tsx

--- components/post/PostAction.tsx
+++ components/post/PostAction.tsx
@@ -2,13 +2,23 @@ import { RiHeartAddLine } from "react-icons/ri"
 import {BiCommentDetail} from "react-icons/bi"
 import { AiOutlineShareAlt } from "react-icons/ai"
 import styles from "./PostAction.module.css"
+import { useRouter } from "next/router"
 
 const PostAction = () => {
 
+    const onClickLikeHandler = async() => {
+        const response = await fetch(`/api/posts/like`, {
+            method: 'POST'
+        })
+
+        if (response.ok) {
+            console.log('Like button clicked!');
+        }
+    }
 
     return (
         <div className={styles.block}>
-            <div>
+            <div onClick={onClickLikeHandler}>
                 <RiHeartAddLine/>
                 <span>10</span>
             </div>
@@ -29,3 +39,7 @@ const PostAction = () => {
 
 
 export default PostAction
+
+function async() {
+    throw new Error("Function not implemented.")
+}
