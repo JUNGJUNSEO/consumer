a45ea0cc03367df9a513eea4385801097474e3ed
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-06 22:17:05 +0900

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useState } from "react"
+import { PropsWithChildren, useEffect, useState } from "react"
 import styles from './Layout.module.css'
 import SearchInput from "../search/SearchInput"
 import Modal from "../ui/Modal"
@@ -9,17 +9,13 @@ import RoundButton from "../ui/Button/RoundButton"
 import UserMenu from "./UserMenu"
 import AuthFormContainer from "@/containers/AuthFormContainer"
 
-interface LayoutProps {
-    user: {
-        id: string
-        loggedIn: boolean
-    }
-}
 
 
-const Layout = ({ user, children }: PropsWithChildren<LayoutProps>) => {
+const Layout = ({ children }: PropsWithChildren) => {
     const [showModal, setShowModal] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
+    const [showShadow, setShowShadow] = useState(false);
+    const [user, setUser] = useState({id: '', loggedIn: false})
    
     const modalHandler = () => {
         setShowModal((preState) => !preState);
@@ -28,6 +24,27 @@ const Layout = ({ user, children }: PropsWithChildren<LayoutProps>) => {
     const menuHandler = () => {
         setShowMenu((preState) => !preState);
     }
+
+    useEffect(() => {
+        function handleScroll() {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            setShowShadow(scrollTop > 0);
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+
+
+    useEffect(() => {
+        async function fetchSession() {
+            const response = await fetch('api/session')
+            const data = await response.json()
+            setUser(data)
+        }
+
+        fetchSession()
+    }, [])
 
     if (showMenu && !user.loggedIn ) {
         menuHandler()
@@ -41,28 +58,41 @@ const Layout = ({ user, children }: PropsWithChildren<LayoutProps>) => {
                     <AuthFormContainer onCloseAuthModal = {modalHandler}/>
                 </>
             )}
-            <div className={styles.layout}>
+            <div className={showShadow ? `${styles.shadow} ${styles.layout}` :  styles.layout}>
                 <Logo/>
                 <div className={styles.search}>
                     <SearchInput/>
                 </div>
-                {user.loggedIn ? (
-                    <div className={styles.user}>
-                        <div className={styles.newPost}>
-                            <RoundButton >
-                                <Link href="/new-post">비교 상품 만들기</Link>
-                            </RoundButton>
-                        </div>
+                <div className={styles.user}>
+                    {user.loggedIn ? (
+                        <>
+                            <div className={styles.newPost}>
+                                <RoundButton >
+                                    <Link href="/new-post">비교 상품 만들기</Link>
+                                </RoundButton>
+                            </div>
 
-                        <UserIcon onClickMenu={menuHandler}/>
-                        {showMenu && <UserMenu/>}
+                            <UserIcon onClickMenu={menuHandler}/>
+                            {showMenu && <UserMenu/>}
+                        
+                        </>
+
         
-                    </div>
-                ) : (
-                    <RoundButton>
-                        <div onClick={modalHandler}>로그인</div>
-                    </RoundButton>
-                )}
+                    ) : (
+                        <>
+                            <Link 
+                                className={styles.joinButton} 
+                                href={"/join"}
+                            >
+                                회원 가입
+                            </Link>
+                            <RoundButton>
+                            <div onClick={modalHandler}>로그인</div>
+                            </RoundButton>
+
+                        </>
+                    )}
+                </div>
             </div>
             <main className={styles.main}>{children}</main>
         </>
