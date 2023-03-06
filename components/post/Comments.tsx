4b95b33f004c5569c4c107b52985ad0576ee8833
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-06 22:17:11 +0900

    Update Comments.tsx

--- components/post/Comments.tsx
+++ components/post/Comments.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from "@/lib/utils"
 import styles from "./Comments.module.css"
 import { PostCommentProps } from "./PostComment"
 
@@ -8,19 +9,33 @@ const Comments:React.FC<PostCommentProps> = ({comments}) => {
         <div className={styles.container}>
             <ul className={styles.comments}>
                 {comments.map((comment, index) => (
-                    <li key={index}>
-                        <img src={comment.writer.avatarUrl} alt={comment.writer.username}/>
-                            <div>
-                                <div>
-                                    <div className={styles.name}>{comment.writer.username}</div>
-                                    <span className={styles.date}>{comment.createdAt.toString()}</span>
+                    <>
+                        <li className={styles.comment} key={index}>
+                            {comment.writer.avatarUrl ? 
+                                <img 
+                                    className={styles.image}
+                                    src={comment.writer.avatarUrl} 
+                                    alt={comment.writer.username}
+                                /> :
+                                <div className={styles.image}>
+                                    {comment.writer.username}
                                 </div>
-                                <p className={styles.text}>
-                                    {comment.text}
-                                </p>
-                            </div> 
-    
-                    </li>
+                            }
+                            
+                                <div>
+                                    <div className={styles.userInfo}>
+                                        <div className={styles.name}>{comment.writer.username}</div>
+                                        <span className={styles.date}>{formatDate(comment.createdAt.toString())}</span>
+                                    </div>
+                                    <p className={styles.text}>
+                                        {comment.text}
+                                    </p>
+                                </div> 
+        
+                        </li>
+                  
+                    </>
+
 
                 ))}
                 
