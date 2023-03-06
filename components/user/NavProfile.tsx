14d6872ce19f653ebc3ff68c8f3ac1fcdc80b12f
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-06 22:17:52 +0900

    Update NavProfile.tsx

--- components/user/NavProfile.tsx
+++ components/user/NavProfile.tsx
@@ -1,38 +1,51 @@
 import Link from "next/link"
-import Router, { useRouter } from "next/router"
-import { Fragment, useEffect, useState } from "react"
+import { useRouter } from "next/router"
 import styles from "./NavProfile.module.css"
 
-function NavProfile() {
-    const [selectedOption, setSelectedOption] = useState('myposts');
-    const router = useRouter();
- 
-    useEffect(() => {
-        const option = router.asPath.split('/').pop(); 
-        setSelectedOption(option)
-    }, [selectedOption])
+interface NavProfileProps {
+    userPosts: number
+    readPosts: number
+    likePosts: number
+}
 
+const NavProfile:React.FC<NavProfileProps> = ({userPosts, readPosts, likePosts}) => {
+    
+    const router = useRouter();
+
+    const isActive = (href: string): boolean => {
+        return router.asPath === href
+    }
+ 
     return (
-        <Fragment>
+        <>
             <div className={styles.block}>
                 <div className={styles.container}>
                     <ul className={styles.navBar}>
                         <li>
-                            <Link href = {`${router.query.userId}`}>
+                            <Link 
+                                href = {`/${router.query.userId}`}
+                                className = {isActive(`/${router.query.userId}`) ? `${styles.active}`: `${styles.inActive}` }
+                            >
                                 <span className={styles.label}>내 포스트</span>
-                                <span className={styles.count}>0</span>
+                                <span className={styles.count}>{userPosts}</span>
                             </Link>
                         </li>
                         <li>
-                            <Link href = {`${router.query.userId}/like`}>
+                            <Link 
+                                href = {`/${router.query.userId}/like`}
+                                className = {isActive(`/${router.query.userId}/like`) ? `${styles.active}`: `${styles.inActive}` }
+                            >
                                 <span className={styles.label}>좋아요 표시한 포스트</span>
-                                <span className={styles.count}>0</span>
+                                <span className={styles.count}>{likePosts}</span>
                             </Link>
                         </li>
                         <li>
-                            <Link href = {`${router.query.userId}/read`}>
+                            <Link 
+                                href = {`/${router.query.userId}/read`}
+                                className = {isActive(`/${router.query.userId}/read`) ? `${styles.active}`: `${styles.inActive}` }
+                            >
                                 <span className={styles.label}>최근에 읽은 포스트</span>
-                                <span className={styles.count}>0</span>
+                                <span className={styles.count}>{readPosts}</span>
                             </Link>
                         </li>
                 
@@ -42,17 +55,7 @@ function NavProfile() {
                 </div>
 
             </div>
-
-            {/* {selectedOption === 'myposts' && 
-                <div>seo</div>
-            }
-            {selectedOption === 'like' && 
-                <div>seo</div>
-            }
-            {selectedOption === 'read' && 
-                <div>seo</div>
-            } */}
-        </Fragment>
+        </>
 
     )
 }
