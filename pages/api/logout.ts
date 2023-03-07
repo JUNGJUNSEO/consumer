874f78dea8d7a603471b6e728be4a96c918f9055
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-07 21:53:06 +0900

    Update logout.ts

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -2,10 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { withSessionRoute } from "@/lib/withSession";
 
 const logoutRoute = async(req: NextApiRequest, res: NextApiResponse ) => {
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({message: "POST method only"})
+  }
+
   req.session.destroy()
-  
-  if (req.url.includes('/new-post')){
-    res.redirect('/');
+
+  const url = req.headers.referer
+  if (url.includes('/new-post')) {
+    return res.redirect('/')
   }
 
   return res.status(200).json({ success: true });
