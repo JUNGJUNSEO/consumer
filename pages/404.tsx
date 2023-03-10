a5c7f5097bf0d47ea6ab08b28e3b579d3219e60f
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-10 22:48:40 +0900

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link"
+
 function ErrorPage(){
 
 
 
     return (
         <div>
-
-        </div>
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for could not be found.</p>
+            <Link href="/">
+                Go back to the homepage
+            </Link>
+      </div>
     )
 }
 
