154cdc6d94f5c2918b12db9a6b83ac7b90fd59eb
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-13 17:07:50 +0900

    Update join.tsx

--- pages/join.tsx
+++ pages/join.tsx
@@ -1,14 +1,11 @@
 import JoinForm from "@/components/auth/JoinForm"
-import Post from "@/components/post/PostTemplate"
 
 function JoinPage() {
-
 
     return (
         <JoinForm/>
         
     )
 }
-
 
 export default JoinPage
