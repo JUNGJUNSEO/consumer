9753eaff6f7372a243c5ed44decbf397f658e376
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-13 17:07:23 +0900

    Create PostDesciption.tsx

---
+++ components/post/PostDesciption.tsx
@@ -0,0 +1,40 @@
+import { AiFillCheckCircle } from 'react-icons/ai';
+import styles from './PostDescription.module.css'
+
+
+interface PostDescriptionProps {
+    files: string[];
+    texts: string[];
+    ownerPick: number;
+    description: string;
+  }
+  
+
+const PostDescription:React.FC<PostDescriptionProps> = ({ files, texts, ownerPick, description }) => {
+
+    return (
+        <div className={styles.block}>
+            <div className={styles.wrapper}>
+                <div className={styles.image}>
+                    <img src={`/images/${files[ownerPick]}`}/>
+                    <AiFillCheckCircle />
+
+                </div>
+                <div className={styles.description}>
+                    <h4>사용자 선택-{texts[0].split(',')[ownerPick]}</h4>
+                    <p>
+                        {description}
+                    </p>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    )
+
+}
+
+
+export default PostDescription
