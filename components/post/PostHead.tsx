0625df8f707091edd9a86388a65c9f7753cd20ac
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2023-03-13 17:07:28 +0900

    Update PostHead.tsx

--- components/post/PostHead.tsx
+++ components/post/PostHead.tsx
@@ -1,6 +1,7 @@
 import TagList from "../common/TagList"
 import classes from "./PostHead.module.css"
 import { formatDate } from "@/lib/utils"
+import Link from "next/link"
 
 interface PostHeadProps{
     userId: string
@@ -17,7 +18,7 @@ const PostHead:React.FC<PostHeadProps> = ({userId, title, createdAt, tags}) => {
                 <h1 className={classes.title}>{title}</h1>
                 <div className={classes.info}>
                     <div>
-                        By <a>{userId}</a>
+                        By <Link href={`/${userId}`}>{userId}</Link>
                     </div>
                     <div style={{ borderLeft: "2px solid",  height: "16px", margin: "0 1rem"}} />
                     <span>{formatDate(createdAt)}</span>
